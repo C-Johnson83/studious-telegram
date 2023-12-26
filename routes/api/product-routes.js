d78d054fc1ce80cf97c27f8d2a62af03d68e2b1e
chris <chrisj21283@gmail.com>
2023-12-26 18:58:37 -0500

    get by product id route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -22,10 +22,27 @@ router.get('/', async (req, res) => {
   });
 
 // get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
+router.get('/:id', async (req, res) => {
+    // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const product = await Product.findOne({
+      
+      where: {
+        id: req.params.id,
+      },
+      include: [
+        { model: Category },
+        { model: Tag, through: ProductTag }
+      ]
+    });
+    res.status(200).json(product);
+  } catch (err) {
+    console.error(err);
+    res.status(400).json(err);
+  }
 });
+
 
 // create new product
 router.post('/', (req, res) => {

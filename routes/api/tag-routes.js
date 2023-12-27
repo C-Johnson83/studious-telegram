498543c241b40e2bacb42790f6fc6990546c5893
chris <chrisj21283@gmail.com>
2023-12-26 19:35:47 -0500

    get tags route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,10 +3,24 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    const tags = await Tag.findAll({
+      include: [
+        {
+          model: Product,
+          through: ProductTag,
+        },
+      ],
+    });
+    res.status(200).json(tags);
+  } catch (err) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
+
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`

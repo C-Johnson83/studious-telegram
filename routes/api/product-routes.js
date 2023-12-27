e1099783d340a752a298f00ae8561706504e2471
chris <chrisj21283@gmail.com>
2023-12-26 19:26:24 -0500

    add product delete route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -117,8 +117,22 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const bounced = await Product.destroy({
+      where: { id: req.params.id }
+    });
+    if (bounced > 0) {
+      res.status(200).send();
+    } else {
+      res.status(404).json({ message: 'Product not found' });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
 });
+
 
 module.exports = router;
